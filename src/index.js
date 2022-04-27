ccb44c526f44c5dfc5bac35cc8b84925f2538948
Weslen Mendes <weslenmends@gmail.com>
2022-04-27 09:59:43 -0300

    feat: set up the development environment

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(json());
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on: http://localhost:${port}/`);
+});
