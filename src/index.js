ca5725797664d90286a363357b07fa122aaf2981
Weslen Mendes <weslenmends@gmail.com>
2022-04-28 10:48:21 -0300

    feat: implemented the messaging feature

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
 
+const messageSchema = Joi.object({
+  to: Joi.string().required(),
+  text: Joi.string().required(),
+  type: Joi.string().pattern(new RegExp("^(private_)?message$")).required(),
+});
+
+const participantSchema = Joi.string().required();
+
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
   const { error } = Joi.string().required().validate(name);
@@ -64,6 +72,44 @@ app.get("/participants", async (req, res) => {
     res.send(allParticipants);
   } catch (e) {
     console.log("Ocorreu um erro: ", e);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+  const validateMessage = messageSchema.validate(req.body);
+  const validateParticipant = participantSchema.validate(user);
+
+  if (validateMessage.error || validateParticipant.error) {
+    res.status(422).send("Ocorreu um erro no formato da mensagem.");
+    return;
+  }
+
+  try {
+    const usernameSanitized = sanitizeString(user);
+    const userExists = await db
+      .collection("participants")
+      .findOne({ name: usernameSanitized });
+
+    if (!userExists) {
+      res.status(422).send("Esse usuário não existe.");
+      return;
+    }
+
+    const message = {
+      to: sanitizeString(to),
+      text: sanitizeString(text),
+      type: sanitizeString(type),
+      from: usernameSanitized,
+      time: dayjs().format("HH:mm:ss"),
+    };
+
+    await db.collection("messages").insertOne(message);
+    res.sendStatus(201);
+  } catch (e) {
+    console.error(e);
     res.sendStatus(500);
   }
 });
